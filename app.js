3c41191e61a6ddc0c54b2b2057ad86ef1c96a667
Prakhar <prakhar.gupta.1106@gmail.com>
2021-05-15 15:48:42 +0530

    Connection Established, Register Form created

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const app = express();
 
 //middleware
@@ -9,6 +11,9 @@ app.use(express.json());
 
 //view engine
 app.set('view engine', 'ejs');
+
+//bodyparser
+app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb+srv://prakhar:C7GNob3mCp7wRgkq@cluster0.6nuob.mongodb.net/Cerial-Killers?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
   console.log('Connection Successful');
@@ -31,28 +36,59 @@ const users = mongoose.model("users", userSchema);
 
 //routes
 app.get('/', (req,res) => {
-    //dummy object (testing purpose)
-    let content={
-    name: "Ojaswi Gupta",
-    email: "ojaswi.2020ca051@mnnit.ac.in",
-    password: "12345",
-    degree: "MCA",
-    college: "MNNIT",
-    gradyear: "2023",
-    linkedin: ".../coder-oj",
-    result: [{prediction: "System Analyst",date: Date().substring(4,21) + " IST"}, {prediction: "Product Manager",date: Date().substring(4,21) + " IST"}]   
-    };
-      //creating new document for each new user
-      const user= new users(content);
-      console.log(content);// console (testing)
-      //data saved to db
-      user.save();
+    // //dummy object (testing purpose)
+    // let content={
+    // name: "Ojaswi Gupta",
+    // email: "ojaswi.2020ca051@mnnit.ac.in",
+    // password: "12345",
+    // degree: "MCA",
+    // college: "MNNIT",
+    // gradyear: "2023",
+    // linkedin: ".../coder-oj",
+    // result: [{prediction: "System Analyst",date: Date().substring(4,21) + " IST"}, {prediction: "Product Manager",date: Date().substring(4,21) + " IST"}]   
+    // };
+    //   //creating new document for each new user
+    //   const user= new users(content);
+    //   console.log(content);// console (testing)
+    //   //data saved to db
+    //   user.save();
 
-      res.send("data Inserted");
-    //res.render('home');
+      //res.send("Welcome");
+    res.render('home');
 });
 
-app.post('/', )
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    let user_details = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    degree: req.body.degree,
+    college: req.body.college,
+    gradyear: req.body.gyear,
+    linkedin: req.body.linkedin,
+    };
+
+    const user= new users(user_details);
+    console.log(user_details);// console (testing)
+      //data saved to db
+      user.save(function(err){
+          if(err){
+              console.log(err);
+          }
+          else{
+            console.log("Data Inserted Successfully");
+            //alert("Data Inserted Successfully");
+          }
+
+      });
+
+    res.render('login');
+});
+
 app.use(authRoutes);
 
 const port = 8000;
